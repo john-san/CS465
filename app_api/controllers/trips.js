cb95de2150379fb17602d2fa81a0eb738b1ec499
John Nguyen <npjohn92@gmail.com>
2023-10-11 17:30:33 -0700

    add angular admin UI w/ add+edit capability

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -27,7 +27,61 @@ const tripsFindByCode = async (req, res) => {
   });
 }
 
+const tripsAddTrip = async (req, res) => {
+  Model.create({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description,
+  }, (err, trip) => {
+    if (err) {
+      res.status(400).json(err) // bad request, invalid content
+    } else {
+      res.status(201).json(trip) // created
+    }
+  })
+}
+
+const tripsUpdateTrip = async (req, res) => {
+  console.log(req.body);
+  Model.findOneAndUpdate({ code: req.params.tripCode }, {
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description,
+  }, { new: true })
+  .then(trip => {
+    if (!trip) {
+      return res.status(404).send({
+        message: "Trip not found with code " + req.params.tripCode
+      });
+    }
+    res.send(trip);
+  }).catch(err => {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "Trip not found with code " + req.params.tripCode
+      });
+    }
+    return res.status(500).send({
+      message: "Error updating trip with code " + req.params.tripCode
+    });
+  })
+
+}
+
+
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 const tripsController = require("../controllers/trips");
 
-router.get("/trips", tripsController.tripsList);
+// router.get("/trips", tripsController.tripsList);
+router
+  .route('/trips')
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip);
 
-router.get("/trips/:tripCode", tripsController.tripsFindByCode);
+// router.get("/trips/:tripCode", tripsController.tripsFindByCode);
+router
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindByCode)
+  .put(tripsController.tripsUpdateTrip)
 
 module.exports = router;
